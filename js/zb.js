44774249cdf5e0eb4b8e26399a7a61a9ed0046a5
军仔kong <qq_42974665@gitcode.net>
2024-04-02 17:07:26 +0800

    Signed-off-by: 军仔kong <qq_42974665@gitcode.net>

--- js/zb.js
+++ js/zb.js
@@ -2,9 +2,5 @@
 
 {"name": "国军直播",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/daquan.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/1.jpg"},
  {"name": "国军MV合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/8000gequ.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
- {"name": "国军MTV合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/meil.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/18.jpeg"},
-{"name": "国军香蕉合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/xiangjiao.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/18.jpeg"},
-{"name": "国军SE合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/se.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/18.jpeg"},
-{"name": "国军SETV合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/setv.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/18.jpeg"},
 {"name": "国军MV合集1",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/mv.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"}
 ]
