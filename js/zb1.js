757075c18c38f74ccbf9e3192644618071c74332
ds360360 <k@kgj.cc>
2024-06-23 18:55:59 +0800

    Signed-off-by: ds360360 <k@kgj.cc>

--- js/zb1.js
+++ js/zb1.js
@@ -3,57 +3,19 @@
 
  {"name":"接口7","url":"https://ghproxy.net/raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
- {"name":"接口9","url":"https://iptv.b2og.com/txt/fmml_ipv6.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
- {"name":"接口11","url":"https://iptv.b2og.com/txt/ycl_iptv.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
- { "name":"接口12","url":"http://wz.42web.io/ipv6.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-
- {"name":"IPV4","url":"https://wzsvip.github.io/ipv4.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-
  {"name":"接口16","url":"http://home.jundie.top:81/Cat/tv/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
  {"name":"接口17","url":"http://xhztv.top/zbc.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
- {"name":"六维","url":"https://4key.cn/ln&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-{ "name":"云汐Cr","url":"https://4key.cn/79 &&&http://api.btstu.cn/sjbz/?lx=meizi"},
 { "name":"放牛娃tv","url":"https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"云汐","url":"https://mpimg.cn/down.php/b7db8225ac7b3297bc007c5f40e6936a.txt&8888&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-{"name":"无名","url":"https://bzgz.club/down.php/568cb6cb7440e448fb3cff1b2dafc12d.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"野火","url":"https://www.netpan.cn/down.php/fce05d2a28081ce42edc1c984a51573a.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"王小二TV","url":"https://cc-im-kefu-cos.7moor-fs2.com/im/2768a390-5474-11ea-afc9-7b323e3e16c0/d4fe44c5-107c-4511-af02-aa08fb10dff7/2024-04-25/2024-04-25_17:22:21/1714036941087/98644330/wexiptv.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
- 
-{"name":"呛口小辣椒","url":"https://pan.baidu.re/down.php/e7c63d65837a0ee2595153290baaf19d.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-{"name":"聚合TV","url":"https://mirror.ghproxy.com/https://raw.githubusercontent.com/happybaihao/pan/main/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"一起看","url":"http://tvbox6.com/tv.php?key=2785&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
 {"name":"中国央视","url":"http://xn--wnu643e.x10.mx/live.m3u&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-{"name":"快看","url":"https://cloud.lxweb.cn/f/6GoQiw/0331.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
 {"name":"小黑IPTV","url":"https://d.kstore.space/download/7395/xiaohei.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"小米","url":"http://124.223.177.85:88/svip/直播挺快.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
 {"name":"明日","url":"https://catbox.n13.club/jk/live&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-{"name":"龍TV","url":"https://gitlab.com/tvtg/vip/-/raw/main/log.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"小胡","url":"https://huhuys.top/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
 {"name":"业余TV","url":"http://47.108.190.141/wx/zb/直播源.php&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"Ray","url":"https://xhdwc.tk/tvlive.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-{"name":"Yoursmile","url":"https://152.32.170.60/Yoursmile7/TVBox/raw/branch/master/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
 
 {"name":"安博TV","url":"https://go.ipayyds.xyz/https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
@@ -61,39 +23,22 @@
 
 {"name":"PHP","url":"https://mirror.ghproxy.com/https://raw.githubusercontent.com/happybaihao/pan/main/phpzb.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
-
-
 {"name":"港澳终极版","url":"https://d.kstore.space/download/8209/港澳台终极版.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-      
+     
 {"name":"芸汐","url":"https://d.kstore.space/download/3701/芸汐电视.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
-{"name":"青鱼","url":"https://fel.forxhr.top:2022/down.php/b04945bc6640a33f693de484cf2095b2.txt&3344520&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-        
-        
+{"name":"青鱼","url":"https://fel.forxhr.top:2022/down.php/b04945bc6640a33f693de484cf2095b2.txt&3344520&&&http://api.btstu.cn/sjbz/?lx=meizi"},       
         
 {"name":"芸汐","url":"https://d.kstore.space/download/3701/芸汐电视.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
 
 {"name":"K歌MV","url":"http://我不是.肥猫.live/lives/mv.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
-
 {"name":"六维","url":"http://39.101.135.137:8081/xx3.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
-
-
 
 {"name":"最全","url":"https://jihulab.com/-/snippets/5265/raw/main/.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
 {"name":"俊于","url":"http://home.jundie.top:81/Cat/tv/live.txt&&&http://api.btstu.cn/sjbz/?lx=meizi"},
 
-
-
 {"name":"影视范","url":"https://agit.ai/fantaiying/fmm/raw/branch/main/tv/m3u/global.m3u&&&http://api.btstu.cn/sjbz/?lx=meizi"}
-
 
 ]
