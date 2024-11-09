eb67bad630a7d8ef0de4fb4fafef6fe6a4433235
kong <123@qq.com>
2024-11-09 11:39:01 +0800

    Signed-off-by: kong <123@qq.com>

--- js/zb.js
+++ js/zb.js
@@ -1,8 +1,8 @@
 [
-{"name": "国军直播",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/daquan.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/1.jpg"},
-{"name":"ipv6直播","url":"https://iptv.b2og.com/txt/fmml_ipv6.txt"},
- {"name": "国军MV合集",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/8000gequ.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
-{"name": "国军MV合集1",     "url": "https://gitee.com/ds360/ds/raw/master/ZB/mv.jpg&&&https://gitee.com/ds360/ds/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
+{"name": "国军直播",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/daquan.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/1.jpg"},
+{"name":"每日自动更新","url":"https://ghp.ci/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.m3u"},
+ {"name": "国军MV合集",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/8000gequ.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
+{"name": "国军MV合集1",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/mv.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
 {"name": "小苹果直播",     "url": "http://tipu.xjqxz.top/live1213.txt"},
 {"name":"虎牙","url":"https://live.freetv.top/huyayqk.m3u"},
 {"name":"斗鱼","url":"https://live.freetv.top/douyuyqk.m3u"},
@@ -10,5 +10,5 @@
 {"name":"哔哩哔哩","url":"https://www.goodiptv.club/bililive.m3u"},
 {"name": "D大佬·猫TV",     "url": "http://rihou.cc:555/gggg.nzk"},
 {"name":"收集直播3","url":"http://27.54.236.201:88/app/live.txt"},
-{"name":"KAIXIN","url":"http://rihou.cc:55/lib/kx2024.txt"}
+{"name":"开心2024","url":"http://ttkx.cc:55/lib/kx2024.txt"}
 ]
