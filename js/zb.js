8c43f8b5baa97ad124bfd0e9209416ab6119ed7c
kong <123@qq.com>
2024-12-04 08:57:19 +0800

    Signed-off-by: kong <123@qq.com>

--- js/zb.js
+++ js/zb.js
@@ -3,12 +3,18 @@
 {"name":"每日自动更新","url":"https://ghp.ci/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.m3u"},
  {"name": "国军MV合集",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/8000gequ.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
 {"name": "国军MV合集1",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/mv.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
-{"name": "小苹果直播",     "url": "http://tipu.xjqxz.top/live1213.txt"},
+{"name": "娱乐电影合集",     "url": "https://tv.iill.top/m3u/Live"},
+{"name":"虎牙优","url":"https://www.goodiptv.club/huyayqk.m3u"},
 {"name":"虎牙","url":"https://live.freetv.top/huyayqk.m3u"},
 {"name":"斗鱼","url":"https://live.freetv.top/douyuyqk.m3u"},
 {"name":"YY","url":"https://www.goodiptv.club/yylunbo.m3u?url=https://lunbo.freetv.top"},
 {"name":"哔哩哔哩","url":"https://www.goodiptv.club/bililive.m3u"},
+{"name":"虎牙备用","url":"http://www.52sw.top:678/iptv/huya.m3u"},
+{"name":"斗鱼备用","url":"http://www.52sw.top:678/iptv/douyu.m3u"},
 {"name": "D大佬·猫TV",     "url": "http://rihou.cc:555/gggg.nzk"},
 {"name":"收集直播3","url":"http://27.54.236.201:88/app/live.txt"},
 {"name":"开心2024","url":"http://ttkx.cc:55/lib/kx2024.txt"}
 ]
+
+
+  
