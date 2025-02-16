f105cb0830807bab28919b2e57bdca4f0fede32d
kong <123@qq.com>
2025-02-16 12:22:01 +0800

    Signed-off-by: kong <123@qq.com>

--- js/zb.js
+++ js/zb.js
@@ -1,19 +1,21 @@
 [
 {"name": "国军直播",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/daquan.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/1.jpg"},
-{"name":"每日自动更新","url":"https://ghp.ci/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.m3u"},
+{"name":"每日自动更新","url":"https://ghproxy.net/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.m3u"},
+{"name":"每日自动更","url":"http://175.178.251.183:6689/live.txt"},
  {"name": "国军MV合集",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/8000gequ.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
 {"name": "国军MV合集1",     "url": "https://gitee.com/dskong/kong/raw/master/ZB/mv.jpg&&&https://gitee.com/dskong/kong/raw/master/%E8%B0%83%E8%AF%95%E5%8A%A9%E6%89%8B/3.jpg"},
 {"name": "娱乐电影合集",     "url": "https://tv.iill.top/m3u/Live"},
-{"name":"虎牙优","url":"https://www.goodiptv.club/huyayqk.m3u"},
+{"name":"肥羊虎牙","url":"https://www.goodiptv.club/huyayqk.m3u"},
 {"name":"虎牙","url":"https://live.freetv.top/huyayqk.m3u"},
 {"name":"斗鱼","url":"https://live.freetv.top/douyuyqk.m3u"},
 {"name":"YY","url":"https://www.goodiptv.club/yylunbo.m3u?url=https://lunbo.freetv.top"},
 {"name":"哔哩哔哩","url":"https://www.goodiptv.club/bililive.m3u"},
-{"name":"虎牙备用","url":"http://www.52sw.top:678/iptv/huya.m3u"},
-{"name":"斗鱼备用","url":"http://www.52sw.top:678/iptv/douyu.m3u"},
-{"name": "D大佬·猫TV",     "url": "http://rihou.cc:555/gggg.nzk"},
-{"name":"收集直播3","url":"http://27.54.236.201:88/app/live.txt"},
-{"name":"开心2024","url":"http://ttkx.cc:55/lib/kx2024.txt"}
+{"name":"52虎牙","url":"http://www.52sw.top:678/iptv/huya.m3u"},
+{"name":"52斗鱼","url":"http://www.52sw.top:678/iptv/douyu.m3u"},
+{"name": "收集直播",     "url": "https://objectstorage.ap-chuncheon-1.oraclecloud.com/n/axghnvpkxk5c/b/vircloud/o/ipv6.txt"},
+{"name":"收集直播3","url":"https://m3u.ibert.me/txt/y_g.txt"},
+{"name":"收集科技","url":"https://gitee.com/koukou890/video/raw/master/iptv.m3u"},
+{"name":"收集直播2","url":"https://ghproxy.net/https://raw.githubusercontent.com/Moexin/IPTV/refs/heads/Files/IPTV.m3u"}
 ]
 
 
